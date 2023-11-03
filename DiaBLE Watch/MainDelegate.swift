e8a0e63cde8b33d4dd3761096c800ce13d9ecbf0
Guido Soranzio <7220550+gui-dos@users.noreply.github.com>
2023-11-03 08:22:44 +0100

    Realm: parse temperature and raw data from flatted JSON

--- DiaBLE Watch/MainDelegate.swift
+++ DiaBLE Watch/MainDelegate.swift
@@ -156,7 +156,7 @@ public class MainDelegate: NSObject, WKApplicationDelegate, WKExtendedRuntimeSes
                     log("Bluetooth: retrieved \(peripheral.name ?? "unnamed peripheral")")
                       bluetoothDelegate.centralManager(centralManager, didDiscover: peripheral, advertisementData: [CBAdvertisementDataServiceUUIDsKey: [CBUUID(string: Dexcom.UUID.advertisement.rawValue)]], rssi: 0)
                 } else {
-                    log("Bluetooth: scanning for Libre/Dexcom...")
+                    log("Bluetooth: scanning for a Libre/Dexcom...")
                     status("Scanning for a Libre/Dexcom...")
                     centralManager.scanForPeripherals(withServices: nil, options: nil)
                 }
