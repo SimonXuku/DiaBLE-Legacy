b42fa11a063eb13329dc5d5863568945a0e97f2b
Guido Soranzio <7220550+gui-dos@users.noreply.github.com>
2023-11-16 22:24:04 +0100

    watchOS 10: fix spacing in Dexcom BLE Setup

--- DiaBLE Watch/Views/Details.swift
+++ DiaBLE Watch/Views/Details.swift
@@ -281,6 +281,7 @@ struct Details: View {
 
                         HStack {
                             Text("Transmitter Serial")
+                            Spacer(minLength: 32)
                             TextField("Transmitter Serial", text: $settings.activeTransmitterSerial)
                                 .multilineTextAlignment(.trailing)
                                 .foregroundColor(.blue)
@@ -288,6 +289,7 @@ struct Details: View {
 
                         HStack {
                             Text("Sensor Code")
+                            Spacer(minLength: 32)
                             TextField("Sensor Code", text: $settings.activeSensorCode)
                                 .multilineTextAlignment(.trailing)
                                 .foregroundColor(.blue)
